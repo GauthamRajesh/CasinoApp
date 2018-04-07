40111505dd397c6aecdaf57e6ea568dee33c571e
pkalavath <pranav.kalavath@gmail.com>
2018-04-06 21:59:24 -0700

    update #4

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+function initializeApp() {
+    var config = {
+        apiKey: "AIzaSyDtWjIn8OmxCmdLi2Wuq2FLtmJOHObS1-E",
+        authDomain: "casinoapptest.firebaseapp.com",
+        databaseURL: "https://casinoapptest.firebaseio.com",
+        projectId: "casinoapptest",
+        storageBucket: "casinoapptest.appspot.com",
+        messagingSenderId: "139922166688"
+    };
+    firebase.initializeApp(config);
+}
+
